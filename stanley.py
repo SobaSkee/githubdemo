07015ee193436be49734854149969fbd77bec638
SobaSkee <kestanley101@gmail.com>
2023-10-25 21:49:56 -0400

    Updated project

--- stanley.py
+++ stanley.py
@@ -1,9 +1,5 @@
-# The password encoder should take in an 8-digit password in string format containing only integers.
-# After passing the password into the encoder, the encoder stores the encoded password to a new
-# variable, with each digit being shifted up by 3 numbers.
-# Examples:
-# “12345555” will become “45678888” after encoding.
-# “00009962” will become “33332295” after encoding.
+# Lab 6
+# Stanley Ke
 
 def print_menu():
     print("Menu")
