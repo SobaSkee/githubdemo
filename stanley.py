4bc80415677fff9932170d83512f4cd01eeda4e8
SobaSkee <kestanley101@gmail.com>
2023-10-25 20:39:23 -0400

    Updated project

--- stanley.py
+++ stanley.py
@@ -13,16 +13,16 @@ def print_menu():
     print("3. Quit")
 
 
-def encode(to_decode):
+def encode(to_encode):
     encoded_password = ''
     my_dict = {"1": "4", "2": "5", "3": "6", "4": "7", "5": "8", "6": "9", "7": "1", "8": "2", "9": "3"}
-    for element in to_decode:
+    for element in to_encode:
         encoded_password += my_dict[element]
+    return encoded_password
 
-
-def decode():
+def decode(to_decode):
+    # finish this function
     pass
-
 
 def main():
     continue_program = True
@@ -30,14 +30,14 @@ def main():
     while continue_program:
         print_menu()
         user_input = int(input("Please enter an option: "))
-
         if user_input == 1:
             # example is 12345555 and encode to 45678888
-            to_decode = input("Please enter your password to encode: ")
-            encode(to_decode)
+            to_encode = input("Please enter your password to encode: ")
+            encoded_password = encode(to_encode)
             print("Your password has been encoded and stored!")
         elif user_input == 2:
-            pass
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
         elif user_input == 3:
             continue_program = False
 
